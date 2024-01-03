5ca4d835280903c55614efcd8ef2b55642e21dfd
JosieEsi <josephine.kpodo@amalitech.org>
2024-01-03 01:50:54 +0000

    created the pages folder to contain information on all countries

---
+++ src/pages/Countries/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles'
+import { Input } from '../../components/Input'
+
+export const Countries = () => {
+    return (
+        <C.CountriesArea>
+            <Input/>
+        </C.CountriesArea>
+    )
+}
